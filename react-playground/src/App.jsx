c2af0f238445ea467a0512ed45c02ca641a0ce38
Lei Justine <clemente.leijustine@gmail.com>
2026-09-16 16:45:31 +0800

    Initial React learning setup

---
+++ react-playground/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+ 
+  return(
+    <h3>Lei Justine Clemente</h3>
+  )
+}
+
+export default App
